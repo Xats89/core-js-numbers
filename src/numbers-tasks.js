3f107764f145585a691c96520da4a7d0abebb544
Khanetski Yayhen <xats89@gmail.com>
2023-12-15 14:08:18 +0300

    done some numbers tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,11 +105,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const xy = x1 * y1 + x2 * y2;
-  const x = Math.sqrt(x1 ** 2 + x2 ** 2);
-  const y = Math.sqrt(y1 ** 2 + y2 ** 2);
-  return Math.acos(xy / (x * y)) * (Math.PI / 180);
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -125,8 +122,10 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  if (!value) return value;
+  const str = String(value);
+  return Number(str.slice(-1));
 }
 
 /**
@@ -140,8 +139,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -157,8 +156,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -178,8 +177,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) return num;
+  return Math.round(Math.floor(num) / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -199,8 +199,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let result = 0;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) result += 1;
+  }
+  return result === 0;
 }
 
 /**
@@ -218,8 +222,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return value / 1 ? value : def;
 }
 
 /**
@@ -233,8 +237,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
